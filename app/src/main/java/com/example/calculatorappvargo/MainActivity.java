ade45ecec17c72b4d3d216a33b6ef0a148656d59
Chase847 <113717852+Chase847@users.noreply.github.com>
2023-09-11 11:58:33 -0500

    Finshed all contraints most of addDigit function need to get calculate done next.

--- app/src/main/java/com/example/calculatorappvargo/MainActivity.java
+++ app/src/main/java/com/example/calculatorappvargo/MainActivity.java
@@ -9,6 +9,12 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     String inputTVDisplay = "";
+    String firstNum = "";
+    String secondNum = "";
+    Boolean isSecondNum = false;
+    Boolean isFirstClick = true;
+    String sign = "";
+
 
     TextView inTV;
 
@@ -22,38 +28,130 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void addDigit(View v) {
-        String output = "";
-        if(v.getId() ==  R.id.button1){
-            output = inputTVDisplay + "1";
-        }
-        else if(v.getId() ==  R.id.button2){
-            output = inputTVDisplay + "2";
-        }
-        else if(v.getId() ==  R.id.button3){
-            output = inputTVDisplay + "3";
-        }
-        else if(v.getId() ==  R.id.button4){
-            output = inputTVDisplay + "4";
-        }
-        else if(v.getId() ==  R.id.button5){
-            output = inputTVDisplay + "5";
-        }
-        else if(v.getId() ==  R.id.button6){
-            output = inputTVDisplay + "6";
-        }
-        else if(v.getId() ==  R.id.button7){
-            output = inputTVDisplay + "7";
-        }
-        else if(v.getId() ==  R.id.button8){
-            output = inputTVDisplay + "8";
-        }
-        else if(v.getId() ==  R.id.button9){
-            output = inputTVDisplay + "9";
+        if (!isSecondNum) {
+            if (v.getId() == R.id.button1) {
+                inputTVDisplay = inputTVDisplay + "1";
+                firstNum += "1";
+            } else if (v.getId() == R.id.button2) {
+                inputTVDisplay = inputTVDisplay + "2";
+                firstNum += "2";
+            } else if (v.getId() == R.id.button3) {
+                inputTVDisplay = inputTVDisplay + "3";
+                firstNum += "3";
+            } else if (v.getId() == R.id.button4) {
+                inputTVDisplay = inputTVDisplay + "4";
+                firstNum += "4";
+            } else if (v.getId() == R.id.button5) {
+                inputTVDisplay = inputTVDisplay + "5";
+                firstNum += "5";
+            } else if (v.getId() == R.id.button6) {
+                inputTVDisplay = inputTVDisplay + "6";
+                firstNum += "6";
+            } else if (v.getId() == R.id.button7) {
+                inputTVDisplay = inputTVDisplay + "7";
+                firstNum += "7";
+            } else if (v.getId() == R.id.button8) {
+                inputTVDisplay = inputTVDisplay + "8";
+                firstNum += "8";
+            } else if (v.getId() == R.id.button9) {
+                inputTVDisplay = inputTVDisplay + "9";
+                firstNum += "9";
+            } else if (v.getId() == R.id.button0) {
+                inputTVDisplay = inputTVDisplay + "0";
+                firstNum += "0";
+            } else if (v.getId() == R.id.additionButton) {
+                isSecondNum = true;
+                isFirstClick = true;
+                sign = "+";
+            } else if (v.getId() == R.id.subtractionButton) {
+                if(isFirstClick){
+                    firstNum += "-";
+                }
+                else {
+                    isSecondNum = true;
+                    sign = "-";
+                }
+            } else if (v.getId() == R.id.multiplicationButton) {
+                isSecondNum = true;
+                isFirstClick = true;
+                sign = "*";
+            } else if (v.getId() == R.id.divisionButton) {
+                isSecondNum = true;
+                isFirstClick = true;
+                sign = "/";
+            } else if (v.getId() == R.id.enterButton) {
+                calculate(v);
+            } else if (v.getId() == R.id.clearButton) {
+                inputTVDisplay = "";
+                firstNum = "";
+                secondNum = "";
+            } else if (v.getId() == R.id.decimalPointButton) {
+                inputTVDisplay = inputTVDisplay + ".";
+            }
+            if(isFirstClick){
+                isFirstClick = false;
+            }
         }
         else{
-            output = inputTVDisplay + "0";
+            if (v.getId() == R.id.button1) {
+                inputTVDisplay = inputTVDisplay + "1";
+                secondNum += "1";
+            } else if (v.getId() == R.id.button2) {
+                inputTVDisplay = inputTVDisplay + "2";
+                secondNum += "2";
+            } else if (v.getId() == R.id.button3) {
+                inputTVDisplay = inputTVDisplay + "3";
+                secondNum += "3";
+            } else if (v.getId() == R.id.button4) {
+                inputTVDisplay = inputTVDisplay + "4";
+                secondNum += "4";
+            } else if (v.getId() == R.id.button5) {
+                inputTVDisplay = inputTVDisplay + "5";
+                secondNum += "5";
+            } else if (v.getId() == R.id.button6) {
+                inputTVDisplay = inputTVDisplay + "6";
+                secondNum += "6";
+            } else if (v.getId() == R.id.button7) {
+                inputTVDisplay = inputTVDisplay + "7";
+                secondNum += "7";
+            } else if (v.getId() == R.id.button8) {
+                inputTVDisplay = inputTVDisplay + "8";
+                secondNum += "8";
+            } else if (v.getId() == R.id.button9) {
+                inputTVDisplay = inputTVDisplay + "9";
+                secondNum += "9";
+            } else if (v.getId() == R.id.button0) {
+                inputTVDisplay = inputTVDisplay + "0";
+                secondNum += "0";
+            } else if (v.getId() == R.id.additionButton) {
+                isSecondNum = true;
+            } else if (v.getId() == R.id.subtractionButton) {
+                if(isFirstClick){
+                    firstNum += "-";
+                }
+                else {
+                    isSecondNum = true;
+                    sign = "-";
+                }
+            } else if (v.getId() == R.id.multiplicationButton) {
+                isSecondNum = true;
+            } else if (v.getId() == R.id.divisionButton) {
+                isSecondNum = true;
+            } else if (v.getId() == R.id.enterButton) {
+                calculate(v);
+            } else if (v.getId() == R.id.clearButton) {
+                inputTVDisplay = "";
+                firstNum = "";
+                secondNum = "";
+            } else if (v.getId() == R.id.decimalPointButton) {
+                inputTVDisplay = inputTVDisplay + ".";
+            }
         }
-        inputTVDisplay = output;
+
         inTV.setText(inputTVDisplay);
+    }
+
+    public void calculate(View v){
+        double fNum = firstNum.;
     }
 }
